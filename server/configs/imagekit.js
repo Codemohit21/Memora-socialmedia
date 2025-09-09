ff112ccb0adc1ec266bc586aba323433e3ff29f3
Codemohit21 <123625047+Codemohit21@users.noreply.github.com>
2025-09-10 02:56:20 +0530

    Update imagekit.js

--- server/configs/imagekit.js
+++ server/configs/imagekit.js
@@ -17,9 +17,10 @@
 import ImageKit from "imagekit";
 
 var imagekit = new ImageKit({
-    publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
-    privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
-    urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
+    
+    publicKey: 'public_+8ZiurdqlAfYQG4R8G8T7mAimfE=',
+    privateKey: 'private_0sav+7A6UTvG1hFy5JDFyZI0nOg=',
+    urlEndpoint: 'https://ik.imagekit.io/xiwgtjkp4',
 });
 
 export default imagekit;
